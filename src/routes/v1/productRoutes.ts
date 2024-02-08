2e626ac34b32cc303ae343d14a9088a0fa1059fb
Lucas Ferrero <lucasdavidferrero@gmail.com>
2024-02-08 17:27:45 -0300

    estableciendo flujo de trabajo y arquitectura básica para documentar la API con Swagger y Open API

--- src/routes/v1/productRoutes.ts
+++ src/routes/v1/productRoutes.ts
@@ -10,6 +10,5 @@ router
 export default router
 
 
-
 // https://dev.to/kabartolo/how-to-document-an-express-api-with-swagger-ui-and-jsdoc-50do
 // https://blog.logrocket.com/write-scalable-openapi-specification-node-js/
